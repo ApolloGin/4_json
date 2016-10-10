bf08fa78411938a3c666cab55a99d67c8e4fd099
Toxagin <toxagin@gmail.com>
2016-10-10 19:58:22 +0300

    I did it

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,16 @@ import json
 
 
 def load_data(filepath):
-    pass
+    data = None
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, indent = 2, ensure_ascii = False)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(input('Введите имя файла с json-ом: '))
+    print(pretty_print_json(data))
